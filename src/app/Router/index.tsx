96a618733e12dbc8fc146379a8eac1845dc77e3f
Matheus Calixto <matheuscalixto8@gmail.com>
2024-07-11 20:21:30 -0300

    feat: configuracao do gerenciamento de rotas da aplicacao

---
+++ src/app/Router/index.tsx
@@ -0,0 +1,23 @@
+import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Loading from '@views/components/Loading';
+
+import { routes } from './routes';
+
+const Home = lazy(() => import('@views/pages/Home'));
+const Product = lazy(() => import('@views/pages/Product'));
+
+const Router = () => {
+  return (
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path={routes.home} element={<Home />} />
+        <Route path={routes.createProduct} element={<Product />} />
+        <Route path={routes.editProduct} element={<Product />} />
+      </Routes>
+    </Suspense>
+  );
+};
+
+export default Router;
---
+++ src/app/Router/routes.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/prefer-default-export
+export const routes = {
+  home: '/',
+  createProduct: '/registrar-produto',
+  editProduct: '/editar-produto',
+};
